7db0f3ce4538f5ab6f66bb0b7b6fe4d48281e071
Myra Ophelia <84265939+MyraOphelia@users.noreply.github.com>
2024-06-06 16:49:44 +0800

    saveDrawing
    - Here is my part for saveDrawing

---
+++ saveDrawing.java
@@ -0,0 +1,11 @@
+
+
+
+public void saveDrawing() {
+    try {
+        ImageIO.write(canvas, "PNG", new File("drawing.png"));
+        JOptionPane.showMessageDialog(this, "Drawing saved successfully!");
+    } catch (IOException ex) {
+        JOptionPane.showMessageDialog(this, "Error saving drawing!", "Error", JOptionPane.ERROR_MESSAGE);
+    }
+}
